88f245a9812535cb9b903c20b1ba7460b9295339
Viswa-28 <viswadiago28@gmail.com>
2025-06-06 18:31:55 +0530

    #

--- js/product.js
+++ js/product.js
@@ -1,38 +1,25 @@
-// $('.bi-chevron-right').click(function () {
-//     $('.carousel-container').css({
-//         'transform': 'translateX(-50%)'
+$('.signup').click(function () {
+  $('.modal').css({
+    'display': 'flex'
+  });
+});
 
-//     });
+
+$('.close-modal').click(function () {
+  $('.modal').css({
+    'display': 'none'
+  });
+});
+
+let width=$(document).innerWidth();
+if (width<600) {setTimeout(() => {
+  $('.modal').css({
+    'display': 'flex'
+  });
+}, 8000);
+
   
-// });
-// $('.bi-chevron-left').click(function () {
-//     $('.carousel-container').css({
-//         'transform': 'translateX(0%)'
-//     });
-  
-// });
-// let currentIndex = 0;
-// const cardsPerView = 3;
-// const totalCards = $('.product-card').length;
-
-// $('.next').click(function () {
-//   if (currentIndex < totalCards - cardsPerView) {
-//     currentIndex++;
-//     updateCarousel();
-//   }
-// });
-
-// $('.prev').click(function () {
-//   if (currentIndex > 0) {
-//     currentIndex--;
-//     updateCarousel();
-//   }
-// });
-
-// function updateCarousel() {
-//   const cardWidth = $('.product-card').outerWidth(true);
-//   $('.carousel-track').css('transform', `translateX(-${cardWidth * currentIndex}px)`);
-// }
+}
 
 
 $('.size').click(function () {
@@ -43,20 +30,25 @@ $('.size').click(function () {
 
 // count
 
-let ubDate=$('.count p').val==1;
+let upDate=$('.count p').val==1;
 $('.bi-plus').click(function () {
-  ubDate++;
-  $('.count p').text(ubDate);
+  upDate++;
+  $('.count p').text(upDate);
+   
 });
 $('.bi-dash').click(function () {
-  ubDate--;
-  $('.count p').text(ubDate);
-});
-// if (ubDate=1) {
-//  $('.bi-dash').removeClass('bi-dash');
-  
-// }
-// elseif(ubDate>1) ;{
-//    $('.bi-dash').addClass('bi-dash');
-// };
+  upDate--;
+  // alert(upDate);
+  $('.count p').text(upDate);
  
+ 
+ 
+});
+
+ if (upDate<2) {
+ 
+  $('.bi-dash').addClass('disable');
+ }
+ else if (upDate>1) {
+  $('.bi-dash').removeClass('disable');
+ }
