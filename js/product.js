1ed97edd1ab3a1fa79065cbbc45c361c61c8b520
Viswa-28 <viswadiago28@gmail.com>
2025-06-08 20:28:16 +0530

    second-final

--- js/product.js
+++ js/product.js
@@ -1,54 +1,62 @@
-$('.signup').click(function () {
-  $('.modal').css({
-    'display': 'flex'
+let hamburgerIcon = document.querySelector('.hamburger-icon');
+let linksMenu = document.querySelector('.links');
+let icon = document.querySelector('.icon-container');
+let parent = document.querySelector('.nav');
+
+hamburgerIcon.addEventListener("click", () => {
+  linksMenu.classList.toggle("show");
+  icon.classList.toggle("show");
+});
+
+$(".signup").click(function () {
+  $(".modal").css({
+    display: "flex",
   });
 });
 
-
-$('.close-modal').click(function () {
-  $('.modal').css({
-    'display': 'none'
+$(".close-modal").click(function () {
+  $(".modal").css({
+    display: "none",
   });
 });
 
-let width=$(document).innerWidth();
-if (width<600) {setTimeout(() => {
-  $('.modal').css({
-    'display': 'flex'
-  });
-}, 8000);
-
-  
+let width = $(document).innerWidth();
+if (width < 600) {
+  setTimeout(() => {
+    $(".modal").css({
+      display: "flex",
+    });
+  }, 5000);
 }
 
-
-$('.size').click(function () {
-  $('.size').removeClass('active');
-  $(this).addClass('active');
+$(".size").click(function () {
+  $(".size").removeClass("active");
+  $(this).addClass("active");
 });
-
 
 // count
 
-let upDate=$('.count p').val==1;
-$('.bi-plus').click(function () {
-  upDate++;
-  $('.count p').text(upDate);
-   
-});
-$('.bi-dash').click(function () {
-  upDate--;
-  // alert(upDate);
-  $('.count p').text(upDate);
- 
- 
- 
-});
+ let upDate = parseInt($(".count p").text());
 
- if (upDate<2) {
- 
-  $('.bi-dash').addClass('disable');
- }
- else if (upDate>1) {
-  $('.bi-dash').removeClass('disable');
- }
+  function updateCountDisplay() {
+    $(".count p").text(upDate);
+    if (upDate <= 1) {
+      $(".bi-dash").addClass("disable");
+    } else {
+      $(".bi-dash").removeClass("disable");
+    }
+  }
+
+  $(".bi-plus").click(function () {
+    upDate++;
+    updateCountDisplay();
+  });
+
+  $(".bi-dash").click(function () {
+    if (upDate > 1) {
+      upDate--;
+      updateCountDisplay();
+    }
+  });
+  updateCountDisplay();
+
